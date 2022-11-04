c0f3b6e9de789fed3fa1463ed6a731033cc3f730
Goat <dpajbk@gmail.com>
2022-11-04 19:15:51 -0400

    get one product route added

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -38,12 +38,20 @@ router.get('/', async (req, res) => {
 // get one product
 router.get('/:id', async (req, res) => {  
   try {
-  const userData = await User.findByPk(req.params.id);
-  if (!userData) {
-    res.status(404).json({ message: 'No user with this id!' });
+  const productData = await Product.findByPk(req.params.id,{
+    include: [
+      {
+        model: Category
+      },
+      // {
+      //   model: Tag,
+      // }
+    ]});
+  if (!productData) {
+    res.status(404).json({ message: 'No product with this id!' });
     return;
   }
-  res.status(200).json(userData);
+  res.status(200).json(productData);
 } catch (err) {
   res.status(500).json(err);
 }
