8464e87b6260913b7a6b57a4fe41b75a9664d8d7
Goat <dpajbk@gmail.com>
2022-11-04 19:12:34 -0400

    get all products works except for tag data

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,18 +1,55 @@
 const router = require('express').Router();
+const sequelize = require('sequelize');
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [
+        {
+          model: Category,
+        },
+        // {
+        //   model: Tag,
+        // }
+      ]
+    })
+    
+    
+    // query(
+    //   `SELECT p.product_id,
+    //   p.product_name,
+    //   p.price,
+    //   p.stock,
+    //   c.category_name FROM product p 
+    //   JOIN category c on c.category_id = p.category_id
+    //   ORDER BY p.product_id ASC;`)
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find all products
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {  
+  try {
+  const userData = await User.findByPk(req.params.id);
+  if (!userData) {
+    res.status(404).json({ message: 'No user with this id!' });
+    return;
+  }
+  res.status(200).json(userData);
+} catch (err) {
+  res.status(500).json(err);
+}
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  
 });
 
 // create new product
